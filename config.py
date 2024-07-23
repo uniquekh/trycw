42eb8e6797ab303c72bb41e95126cf4408c74be6
Unique <mujeem000@gmail.com>
2024-07-23 15:45:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6710743479:AAGSgL_K5cox7r9vHRNX39mvFbYB9bUvTdE")
+    API_ID = int(os.environ["API_ID", 21179966]
+    API_HASH = os.environ["API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c"]
+    AUTH_USERS = "7326397503"""
+ 
